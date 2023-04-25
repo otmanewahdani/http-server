6abbcf69d31ddd8b037c6cf0f1b93cbd1a734643
Fatima-Zahra Louta <fatimazahralouta@gmail.com>
2023-04-25 22:27:44 +0100

    emplimented URL::checkBadCharacters() & URL::getMatchedLocation()

--- srcs/Request/URL.cpp
+++ srcs/Request/URL.cpp
@@ -2,17 +2,23 @@
 
 #include <URL.hpp>
 
+// init the allowed characters in a url path
+const std::string URL::mAllowedChars =	"0123456789"
+										"abcdefghijklmnopqrstuvwxyz"
+										"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+										"-_.~:/?#[]@!$&'()*+,;=";
 
 URL::URL(ConstServerRef server)
-	:mServer(server)
-	:mValid(true)
-	:mParsed(false) {}
+	:mServer(server),
+	 mValid(true),
+	 mParsed(false),
+	 mLocation(NULL) {}
 
 
 void URL::parse(const std::string& url) {
 
-	if(!mParsed) {
-		parseUrl();
+	if (!mParsed) {
+		parseUrl(url);
 		addFullPath();
 	}
 
@@ -20,17 +26,19 @@ void URL::parse(const std::string& url) {
 
 void URL::parseUrl(const std::string& url) {
 
-	checkBadCharacter(url);
+	checkBadCharacters(url);
 
-	// get the position of the query string in the url
-	size_t pos;
-	pos = getQueryPos(url);
+	if (mValid) {
+		// get the position of the query string in the url
+		size_t pos;
+		pos = getQueryPos(url);
 
-	// add the query string starting from pos if it exists
-	addQueryString(url, pos);
+		// add the query string starting from pos if it exists
+		addQueryString(url, pos);
 
-	// add the path if it matches a mSeerver location
-	addPath(url, pos);
+		// add the path if it matches a mSeerver location
+		addPath(url, pos);
+	}
 
 }
 
@@ -40,7 +48,7 @@ size_t URL::getQueryPos(const std::string& url) {
 
 void URL::addQueryString(const std::string& url, size_t pos) {
 	
-	if(pos != std::string::npos) {
+	if (pos != std::string::npos) {
 		// construct the mQuery string starting 
 			//from pos to the end of the url
 		mQuery = url.substr(pos);
@@ -52,11 +60,71 @@ void URL::addPath(const std::string& url, size_t len) {
 
 	// construct the path string from the beginning 
 		// of the url until the position of query part
-	mPath = substr(0, len);
+	mPath = url.substr(0, len);
 
 	// get the mServer location that matches the mPath
-	mLocation = getMatchedLocation();
+	getMatchedLocation();
 
-	// if the url path doesn't match any location in mServer set NotFound 
+	// if the url path doesn't match any location 
+		// in mServer set NotFound error
+	if (!mLocation) {
+		mValid = false;
+		mStatusCode = StatusCodeHandler::NOT_FOUND;
+	}
 
+}
+
+void URL::checkBadCharacters(const std::string& url) {
+
+	// if a bad request 
+	if (url.find_first_not_of(mAllowedChars) != std::string::npos) {
+		mValid = false;
+		mStatusCode = StatusCodeHandler::BAD_REQUEST;
+	}
+}
+
+void URL::getMatchedLocation() {
+
+	Path TmpPath;
+	TmpPath = mPath + "/";
+	size_t pos;
+
+	while (!TmpPath.empty()) {
+		pos = TmpPath.rfind("/");
+		TmpPath.erase(pos+1);
+		mLocation = mServer.getLocation(TmpPath);
+		TmpPath.erase(pos);
+		mLocation = mServer.getLocation(TmpPath);
+	}
+
+}
+
+void URL::addFullPath() {
+
+	// appends the mPath to the root of mLocation
+		// to get the full path of the requested resource
+		// if no error occurred
+	if (mValid)
+		mFullPath = mLocation->root + mPath;
+
+}
+
+bool URL::isValid() const {
+	return mValid;
+}
+
+const URL::Path& URL::getPath() const {
+	return mPath;
+}
+
+const URL::FullPath& URL::getFullPath() const {
+	return mFullPath;
+}
+
+const URL::QueryString& URL::getQueryString() const {
+	return mQuery;
+}
+
+const URL::StatusCode& URL::getStatusCode() const {
+	return mStatusCode;
 }
