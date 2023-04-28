fc0efdc864306ba4e2198550db4df62ab513ee74
Fatima-Zahra Louta <fatimazahralouta@gmail.com>
2023-04-28 20:19:57 +0100

    implemented RequestChecker::{isAutoindex() ,isUpload(), isDefault(), isContent()}

--- srcs/Request/RequestChecker.cpp
+++ srcs/Request/RequestChecker.cpp
@@ -35,3 +35,62 @@ bool RequestChecker::isValid() {
 	return false;
 
 }
+
+bool RequestChecker::isAutoIndex() {
+
+	// if the the requested resource 
+		// is a directory and the location
+		// enables autoindexing
+	if(mIsDir == false || 
+		mLocation->autoindex == false) {
+		return false;
+	}
+
+	mRequest.setRequestType(Request::AUTOINDEX);
+	return true;
+	
+}
+
+bool RequestChecker::isUpload() {
+
+	// checks if the full path of the requested 
+		// ressource is a directory and the 
+		// location enables uploading 
+	if (mIsDir == false || 
+		mLocation->uploadRoute.empty()) {
+		return false;
+	}
+
+	mRequest.setRequestType(Request::UPLOAD);
+	return true;
+
+}
+
+bool RequestChecker::isDefault() {
+
+	// checks if the location specifies a default 
+		// file to serve in case of get method 
+		// and the path of the request is
+		// a directory otherwise returns false 
+	if (mIsDir == false || 
+		mRequest.getMethod() != Request::GET || 
+		mLocation.defaultFile.empty()) {
+		return false;
+	}
+	
+	mRequest.setRequestType(Request::DEFAULT);
+	return true;
+
+}
+
+bool RequestChecker::isContent() {
+
+	// checks if the path exists and is not a directory
+		// in case of requesting a static file
+	if (mIsPath == false || mIsDir)
+		return false;
+
+	mRequest.setRequestType(Request::CONTENT);
+	return true;
+
+}
