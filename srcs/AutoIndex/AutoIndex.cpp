28bb24141a891983a8216acdc5d7f267f8bac321
Fatima-Zahra Louta <fatimazahralouta@gmail.com>
2023-05-07 22:32:05 +0100

    sets the logic of AutoIndex class and implemented AutoIndex::generate()

---
+++ srcs/AutoIndex/AutoIndex.cpp
@@ -0,0 +1,53 @@
+/* this file contains the implementation of AutoIndex class */
+
+#include <AutoIndex.hpp>
+
+const std::string AutoIndex::mHtmlListingHeader 
+	= "<!DOCTYPE html><html>"
+	"<head><title>Directory Listing</title></head>"
+	"<body><h1>Directory Listing</h1>"
+	"<table style=\"font-size: 25px\">"
+    "<thead><tr><th>Name</th>"
+    "<th>Size</th><th>Last Modified</th>"
+    "</tr></thead><tbody>";
+
+const std::string AutoIndex::mHtmlListingFooter
+	= "</tbody></table>"
+	"</body></html>";
+
+AutoIndex::AutoIndex(const std::string& dirPath, 
+	const std::string& outputFilePath)
+	:mOutputFilePath(outputFilePath) 
+	, mDirPath(dirPath) {}
+
+void AutoIndex::generate() {
+
+	const std::vector<std::string>& dirContent 
+		= getDirContent(mDirPath);
+	
+	std::ofstream listingStream;
+	listingStream.open(mOutputFilePath.c_str(),  
+		std::ofstream::in | std::ofstream::binary);
+	
+	listingStream << mHtmlListingHeader;
+
+	std::vector<std::string>::const_iterator dirEntity;
+	for(dirEntity = dirContent.begin(); dirEntity != dirContent.end(); ++dirEntity) {
+		listingStream << getDirEntityInfo(*dirEntity);
+	}
+
+	listingStream << mHtmlListingFooter;
+
+	listingStream.close();
+
+}
+
+const std::string& AutoIndex::
+	getDirEntityInfo(const std::string& dirEntity) {
+		//<tr>
+		//<th><a href="replaceByRoute()">dirEntity</a></th>
+		//<th>getSize()</th>
+		// gte time
+		//<th>timetostr</th>
+		//</tr>
+}
--- srcs/AutoIndex/AutoIndex.hpp
+++ srcs/AutoIndex/AutoIndex.hpp
@@ -1,10 +1,44 @@
 /* this file contains the definition of the AutoIndex class.
  * This class generates an html listing of a directory that's
  * passed to it. The listing is in the following format:
- *  [File Size Last-Modified]
- * the listing is saved in a file that's passed to this an object
- * of this class before starting the listing generation
+ *  [File Size Last-Modified], and the generated html listing
+ *  will be saved in the file path that is passed to it
  */
+
+#pragma once
 
 #include <fstream>
 #include <utils.hpp>
+#include <vector>
+
+class AutoIndex {
+
+	public:
+		/******* public member functions *******/
+		AutoIndex(const std::string& dirPath, 
+			const std::string& outputFilePath);
+		
+		void generate();
+
+	private:
+		/******* private member objects *******/
+		// the filepath where the generated html
+			// listing will be saved
+		const std::string mOutputFilePath;
+
+		// the input directory that will be listed
+		const std::string mDirPath;
+
+		// contains the first lines of the listing 
+			// html file
+		static const std::string mHtmlListingHeader;
+
+		// contains the ending lines of the listinfg
+			// html file
+		static const std::string mHtmlListingFooter;
+
+		/******* private member functions *******/
+		const std::string& getDirEntityInfo
+			(const std::string& dirEntity);
+};
+
